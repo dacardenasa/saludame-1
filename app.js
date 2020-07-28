884eaaa66f4261f902f295f0c35cf3fafc988c8b
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-28 12:12:11 -0500

    Add query url handler to challenge

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  let requestQuery;
+  req.query.nombre && req.query.nombre.length > 0
+    ? (requestQuery = req.query.nombre)
+    : (requestQuery = "Hola desconocido!");
+  res.send(`<h1>${requestQuery}</h1>`);
+});
+
+app.listen(3000, () => console.log("Listening on port: 3000"));
